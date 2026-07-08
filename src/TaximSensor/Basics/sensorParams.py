6057793670e9db30add245c1f1c9e3f93b675163
Seongjin Bien <seongjin.bien@utn.de>
2026-07-08 15:23:57 +0200

    modify pixmm

--- src/TaximSensor/Basics/sensorParams.py
+++ src/TaximSensor/Basics/sensorParams.py
@@ -1,6 +1,6 @@
 # experimental params
 ball_radius = 4.00/2
-pixmm = 0.055 # OFR gelsight 0.05 # gelsight 0.0295 #0.0266 digit # 0.055 YCB-Slide  # 0.04 for UTN digit
+pixmm = 0.0266 # OFR gelsight 0.05 # gelsight 0.0295 #0.0266 digit # 0.055 YCB-Slide  # 0.04 for UTN digit
 # Seems like the way I set up the sensor is a bit jank, so the values don't actually align all that well........
 
 # OFR Gelsight 32mm x 24mm 
